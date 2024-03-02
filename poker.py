9115e246687078579c567fc27470641377612b78
no31 <liudongzhe031@gmail.com>
2024-03-02 18:28:40 +0300

    OOP

--- poker.py
+++ poker.py
@@ -1,40 +1,71 @@
-import random
 import matplotlib.pyplot as plt
-
-def drawCards():
-    suits = ['1', '2', '3', '4']
-    cards = [str(i) for i in range(1, 14)]
-    deck = [(suit, card) for suit in suits for card in cards]
-    random.shuffle(deck)
-    return deck
+import numpy as np
+import random
 
 
-def prob(num):
-    sameSuit = 0
-    p = []
-    i = 0
-    while i < num:
-        i += 1
-        deck = drawCards()
-        card1 = random.choice(deck)
-        deck.remove(card1)
-        card2 = random.choice(deck)
-        if card1[0] == card2[0]:
-            sameSuit += 1
-        p.append(sameSuit / i)
-    return p
+class poker:
+    def __init__(self):
+        self.suits = [1, 2, 3, 4]
+        self.cards = [i for i in range(1, 14)]
+        # self.drawNum = drawNum
+        self.deck = [(suit, card) for suit in self.suits for card in self.cards]
+        self.totalNum = None
+        self.res = []
+        self.ans = []
 
+    def printCards(self):
+        print(self.res)
+        print(self.ans)
 
-def printGraph(p, num):
-    x = [i for i in range(num)]
-    plt.plot(x, p)
-    plt.title(f'the probability is: {p[-1]}')
-    plt.xlabel('Frequency')
-    plt.ylabel('Probability')
-    plt.show()
+    def shuffleCards(self):
+        np.random.shuffle(self.deck)
+
+    def drawCards(self, drawNum, totalNum):
+        self.totalNum = totalNum
+        tmp = []
+        for i in range(totalNum):
+            t = self.deck
+            for j in range(drawNum):
+                card = random.choice(t)
+                tmp.append(card)
+                t = t.remove(card)
+
+            self.res.append(tmp)
+            tmp = []
+
+    def compare(self):
+        tmp = []
+        for i in range(self.totalNum):
+            t = np.array(self.res[i]).T
+            t = t[0]
+            tmp.append(t)
+        # print(tmp)
+        isSame = 0
+        flag = 0
+        for i in range(self.totalNum):
+            if np.all(tmp[i][0] == tmp[i]):
+                flag += 1
+                isSame += 1
+
+                self.ans.append(isSame / flag)
+            else:
+                flag += 1
+                self.ans.append(isSame / flag)
+
+    def printGraph(self):
+        x = [i for i in range(self.totalNum)]
+        plt.plot(x, self.ans)
+        plt.title(f'the probability is: {self.ans[-1]}')
+        plt.xlabel('Frequency')
+        plt.ylabel('Probability')
+        plt.ylim(0, 0.01)
+        plt.show()
 
 
 if __name__ == "__main__":
-    num = 10000
-    p = prob(num)
-    printGraph(p, num)
+    poker1 = poker()
+    poker1.shuffleCards()
+    poker1.drawCards(drawNum=2, totalNum=10000)
+    poker1.compare()
+    # poker1.printCards()
+    poker1.printGraph()
